2875166bd08074a4fee3d70dfdc7c166ceb7f2d2
Isaac Song <isaacsongs10@gmail.com>
2023-12-05 09:41:03 -0800

    add user cols and pw hash

--- server/models/users.py
+++ server/models/users.py
@@ -8,7 +8,15 @@ class User(db.model):
     __tablename__ = 'users'
 
     # Columns for users Table
-
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    location = db.Column(db.String)
+    bio = db.Column(db.String)
+    genres = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Relationships
 
@@ -20,7 +28,17 @@ class User(db.model):
 
 
     # Password hashing
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("No peeking at the password...")
 
+    @password_hash.setter
+    def password_hash(self, new_password):
+        hashed_password = bcrypt.generate_password_hash(new_password).decode("utf-8")
+        self._password_hash = hashed_password
+
+    def authenticate(self, password_to_check):
+        return bcrypt.check_password_hash(self._password_hash, password_to_check)
 
     # repr
     def __repr__(self):
