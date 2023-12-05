5d49cb2efcd51fb4faa4bd92172b17174c698def
Isaac Song <isaacsongs10@gmail.com>
2023-12-05 15:54:56 -0800

    add follower validations

--- server/models/follower.py
+++ server/models/follower.py
@@ -1,8 +1,6 @@
 from sqlalchemy.orm import validates
 from app_setup import db
-from sqlalchemy.ext.hybrid import hybrid_property
-from app_setup import bcrypt
-from sqlalchemy.ext.associationproxy import association_proxy
+from models.user import User
 
 class Follower(db.Model):
     __tablename__ = 'followers'
@@ -16,7 +14,19 @@ class Follower(db.Model):
     following = db.relationship('User', back_populates='followings', foreign_keys=[following_id])
     
     # Validations
-    
+    @validates('follower_id')
+    def validate_followerid(self, _, value):
+        if value and db.session.get(User, value):
+            return value
+        else:
+            raise ValueError('User ID must be from a valid user')
+        
+    @validates('following_id')
+    def validate_followingid(self, _, value):
+        if value and db.session.get(User, value):
+            return value
+        else:
+            raise ValueError('User ID must be from a valid user')
 
     # repr
     def __repr__(self):
