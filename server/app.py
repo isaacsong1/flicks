03acb5984023a1cbbd122feade8b03fae6532077
Isaac Song <isaacsongs10@gmail.com>
2023-12-06 11:26:39 -0800

    add moviecolls/moviecollbyid routes

--- server/app.py
+++ server/app.py
@@ -16,28 +16,37 @@ from models.movie_collection import MovieCollection
 from models.show_collection import ShowCollection
 
 # Route imports
+#! Authentication
 from routes.auth.check_session import CheckSession
 from routes.auth.login import Login
 from routes.auth.logout import Logout
 from routes.auth.register import Register
-
+#! General routes
+from routes.movie_by_id import MovieById
+from routes.movie_coll_by_id import MovieCollectionById
+from routes.movie_collections import MovieCollections
+from routes.movies import Movies
 from routes.show_by_id import ShowById
-
-
+from routes.show_coll_by_id import ShowCollectionById
+from routes.show_collections import ShowCollections
 from routes.shows import Shows
 from routes.user_by_id import UserById
 from routes.users import Users
 
 # Add resources
+#! Authentication
 api.add_resource(CheckSession, '/checksession')
 api.add_resource(Login, '/login')
 api.add_resource(Logout, '/logout')
 api.add_resource(Register, '/register')
-
-
+#! General routes
+api.add_resource(MovieById, '/movies/<int:id>')
+api.add_resource(MovieCollectionById, 'movie_collections/<int:id>')
+api.add_resource(MovieCollections, '/movie_collections')
+api.add_resource(Movies, '/movies')
 api.add_resource(ShowById, '/shows/<int:id>')
-
-
+api.add_resource(ShowCollectionById, '/show_collections/<int:id>')
+api.add_resource(ShowCollections, '/show_collections')
 api.add_resource(Shows, '/shows')
 api.add_resource(UserById, '/users/<int:id>')
 api.add_resource(Users, '/users')
--- server/routes/movie_coll_by_id.py
+++ server/routes/movie_coll_by_id.py
@@ -1,4 +1,43 @@
-from . import session, Resource
+from . import request,session, Resource
 from models.movie_collection import MovieCollection
 from schemas.movie_coll_schema import MovieCollectionSchema
 from app_setup import db
+
+movie_collection_schema = MovieCollectionSchema(session=db.session)
+
+# Get a collection, Update collection, Delete collection
+class MovieCollectionById(Resource):
+    def get(self, id):
+        if movie_collection := db.session.get(MovieCollection, id):
+            return movie_collection_schema.dump(movie_collection), 200
+        return {'error': 'Could not find that movie collection'}, 404
+
+    def patch(self, id):
+        if movie_collection := db.session.get(MovieCollection, id):
+            try:
+                # Get user input data
+                data = request.json
+                # Validate movie collection information
+                movie_collection_schema.validate(data)
+                # Deserialize data and allow for partial updates
+                updated_movie_collection = movie_collection_schema.load(
+                    data, instance=movie_collection, partial=True
+                )
+                db.session.commit()
+                # Serialize data and package JSON response
+                return movie_collection_schema.dump(updated_movie_collection), 200
+            except Exception as e:
+                db.session.rollback()
+                return {'error': f'MovieCollById Patch error, {str(e)}'}, 400
+        return {'error': 'Could not find movie collection'}, 404
+
+    def delete(self, id):
+        if movie_collection := db.session.get(MovieCollection, id):
+            try:
+                db.session.delkete(movie_collection)
+                db.session.commit()
+                return {'message': f'Movie collection #{id} has been deleted'}, 200
+            except Exception as e:
+                db.session.rollback()
+                return {'error': f'MovieCollById Delete error, {str(e)}'}, 400
+        return {'error': 'Could not find movie collection'}, 404
