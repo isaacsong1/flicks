64a29776b25d73b25a48143d28ab8fd5e9152a11
Isaac Song <isaacsongs10@gmail.com>
2023-12-06 10:53:02 -0800

    add movies/moviebyid routes

--- server/routes/movie_by_id.py
+++ server/routes/movie_by_id.py
@@ -2,3 +2,13 @@ from . import session, Resource
 from models.movie import Movie
 from schemas.movie_schema import MovieSchema
 from app_setup import db
+
+movie_schema = MovieSchema(session=db.session)
+
+# Get one movie
+class MovieById(Resource):
+    def get(self, id):
+        if movie := db.session.get(Movie, id):
+            # movie_schema = MovieSchema()
+            return movie_schema.dump(movie), 200
+        return {'error': 'Could not find that movie'}, 404
--- server/routes/movies.py
+++ server/routes/movies.py
@@ -2,3 +2,11 @@ from . import session, Resource
 from models.movie import Movie
 from schemas.movie_schema import MovieSchema
 from app_setup import db
+
+movies_schema = MovieSchema(many=True, session=db.session)
+
+# Get all movies 
+class Movies(Resource):
+    def get(self):
+        movies = movies_schema.dump(Movie.query)
+        return movies, 200
