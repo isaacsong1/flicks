90387bd7b2b6acc7f44f5aaf2748dc59b0603a3b
Isaac Song <isaacsongs10@gmail.com>
2023-12-11 22:21:57 -0800

    nest media data within collection

--- server/schemas/show_coll_schema.py
+++ server/schemas/show_coll_schema.py
@@ -1,11 +1,13 @@
 from marshmallow import fields, validate, validates, ValidationError
 from models.show_collection import ShowCollection
 from app_setup import ma
+from schemas.show_schema import ShowSchema
 
 class ShowCollectionSchema(ma.SQLAlchemySchema):
     class Meta:
         model = ShowCollection
         load_instance = True
-        fields = ['id', 'name', 'user_id', 'show_id']
+        fields = ['id', 'name', 'user_id', 'show_id', 'show']
 
     name = fields.String(required=True, validate=validate.Length(min=3, max=20))
+    show = fields.Nested(ShowSchema)
