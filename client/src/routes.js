152fa996bbdeeef8923aef20a0dc27bf4a7a24a9
Isaac Song <isaacsongs10@gmail.com>
2023-12-20 20:43:02 -0800

    clean top components client

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,15 +1,10 @@
 import App from './App';
 import Authentication from './features/user/Authentication';
 import ErrorPage from './pages/ErrorPage';
-// import AllMedia from './pages/AllMedia';
 import Discover from './pages/Discover';
 import Connect from './pages/Connect';
 import MyCollection from './pages/MyCollection';
 import Profile from './pages/Profile';
-
-// What routing will look like:
-// Upon directing to flicks => All movie directory (with option to select shows)
-// If time persists => Have an actual home page: Mock tutorial on web app
 
 const routes = [
     {
@@ -21,10 +16,6 @@ const routes = [
                 path: '/',
                 element: <Authentication />
             },
-            // {
-            //     path: '/',
-            //     element: <AllMedia />
-            // },
             {
                 path: '/discover',
                 element: <Discover />,
